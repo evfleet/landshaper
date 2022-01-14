e9a7cf3e518d3c4d80eb9b628a46a2ab0c432ef0
Evan Fleet <evfleet@gmail.com>
2022-01-14 02:37:00 -0500

    build(root): test that monorepo functionality is working

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,7 +1,12 @@
+import { Card } from "@landshaper/common";
 import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const test: Card = {
+  id: "test",
+};
 
 app.listen(() => {
   try {
