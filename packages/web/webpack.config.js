dea4f1da4d25ffceec6e11ecd209c1c211e83fff
Evan Fleet <evfleet@gmail.com>
2022-01-14 17:46:03 -0500

    build(web): initial react app commit

---
+++ packages/web/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HTMLPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: "./src/index.html",
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+};
