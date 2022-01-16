98961281f37f639e33b34b906d1d9f1d7042c656
Evan Fleet <evfleet@gmail.com>
2022-01-15 19:46:51 -0500

    test(root): setup testing for all packages

---
+++ jest.config.ts
@@ -0,0 +1,4 @@
+module.exports = {
+  // Run tests from one or more projects
+  projects: ["<rootDir>/packages/*/jest.config.ts"],
+};
---
+++ packages/web/src/components/App/App.spec.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+
+import App from "./App";
+
+describe("App", () => {
+  it("should render Hello World", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Hello World/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
