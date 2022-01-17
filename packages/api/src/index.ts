9e76faec40ecf86d9949d39a44b3e1ef9d56aea5
Evan Fleet <evfleet@gmail.com>
2022-01-17 16:27:49 -0500

    feat(api): setup logger

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,14 +1,15 @@
 import 'dotenv/config';
 import database from './config/database';
+import logger from './loaders/logger';
 import app from './server';
 
 const port = process.env.PORT || 3000;
 
-app.listen(async () => {
+app.listen(port, async () => {
   try {
     await database.sync({ force: true });
-    console.log(`Server listening on port ${port}`);
+    logger.info(`Server started on port ${port}`);
   } catch (error) {
-    console.error(error);
+    logger.error(error);
   }
 });
