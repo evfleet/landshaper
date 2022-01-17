aa97d2684b2006c32c9537a174cd18d87192e5b7
Evan Fleet <evfleet@gmail.com>
2022-01-17 16:37:53 -0500

    fix(api): remove logging for files

--- packages/api/src/loaders/logger.ts
+++ packages/api/src/loaders/logger.ts
@@ -30,14 +30,7 @@ const format = winston.format.combine(
   winston.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
 );
 
-const transports = [
-  new winston.transports.Console(),
-  new winston.transports.File({
-    filename: 'logs/error.log',
-    level: 'error'
-  }),
-  new winston.transports.File({ filename: 'logs/all.log' })
-];
+const transports = [new winston.transports.Console()];
 
 const logger = winston.createLogger({
   level: level(),
