c5acc15987ae57efe1854626791bc9fb25dc1cad
Evan Fleet <evfleet@gmail.com>
2022-01-17 18:50:23 -0500

    refactor(api): refactor models to import into the sequelize init

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,13 +1,13 @@
 import 'dotenv/config';
-import database from './config/database';
 import logger from './loaders/logger';
+import database from './loaders/sequelize';
 import app from './server';
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, async () => {
   try {
-    await database.sync({ force: true });
+    await database.sequelize.sync({ force: true });
     logger.info(`Server started on port ${port}`);
   } catch (error) {
     logger.error(error);
